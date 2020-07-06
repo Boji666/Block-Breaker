3f443990e709d365bf195df90edd73c3597652f5
Boji666 <danielbojart@gmail.com>
2020-07-07 00:14:01 +0200

    Added sounds to blocks and the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -7,15 +6,22 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddleOne;
     [SerializeField] float posX = 2f;
     [SerializeField] float posY = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     // state
     Vector2 paddleToBallVector;
     bool hasStarted;
+
+    // Cahced compoenent references
+
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         hasStarted = false;
         paddleToBallVector = transform.position - paddleOne.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -42,5 +48,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddleOne.transform.position.x, paddleOne.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[Random.Range(0,ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
